065e3a50b7fd59be54f31ced7722717e57649855
Roger Rosa <rogeritael.contato@gmail.com>
2023-07-11 21:59:00 -0300

    [server] config - instalando dependências do mongoose e criando schema de carrinho

---
+++ server/src/models/Cart.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+    code: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    price: {
+        type: Number,
+        required: true
+    }
+},{
+    timestamps: true,
+});
+
+export default mongoose.model("Cart", schema)
