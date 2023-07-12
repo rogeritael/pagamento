6bdec1bcafff5f0dc202ec4b5b70fd4942353f21
Roger Rosa <rogeritael.contato@gmail.com>
2023-07-11 21:27:38 -0300

    [server] config - setup inicial do projeto, instalando dependências e estrutura de pastas

---
+++ server/src/controllers/CartsController.js
@@ -0,0 +1,7 @@
+class CartsController{
+    async index(req, res){
+        return res.status(200).json({ foo: 'bar' })
+    }
+}
+
+export default new CartsController();
---
+++ server/src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import CartsController from "./controllers/CartsController";
+
+const routes = new Router();
+
+routes.get("/carts", CartsController.index);
+
+export default routes;
