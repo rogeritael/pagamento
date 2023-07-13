e1dc953cf8b784a3fd511aeffd5fcec4e1d81ce1
Roger Rosa <rogeritael.contato@gmail.com>
2023-07-12 22:54:08 -0300

    [server] config - criação da model de Transaction

---
+++ server/src/models/Transaction.js
@@ -0,0 +1,80 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+    cartCode: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    //código da transação
+    code: {
+        type: Number,
+        required: true
+    },
+    //status da transação - iniciada, pendente, aprovada
+    status: {
+        type: String,
+        enum: [ //para garantir que existam apenas esses tipos de status
+            "started",
+             "processing",
+             "pending",
+             "approved",
+             "refused", //cartão recusado
+             "refunded",
+             "chargeback", //quando cancela na operadora
+             "error"
+        ],
+        required: true,
+    },
+    paymentType: {
+        type: String,
+        enum: ["billet", "credit_cart"],
+        required: true
+    },
+    installments: { //parcelas
+        type: Number,
+    },
+    total: {
+        type: Number,
+    },
+    transactionId: {
+        type: String,
+    },
+    processorResponse: { //resposta do adquirent [tid e nit]
+        type: String,
+    },
+    customerEmail :{
+        type: String,
+    },
+    customerName :{
+        type: String,
+    },
+    customerMobile :{
+        type: String,
+    },
+    customerDocument :{
+        type: String,
+    },
+    billingAddress :{
+        type: String,
+    },
+    billingNumber :{
+        type: String,
+    },
+    billingNeighborhood :{
+        type: String,
+    },
+    billingCity :{
+        type: String,
+    },
+    billingState :{
+        type: String,
+    },
+    billingZipCode :{
+        type: String,
+    },
+},{
+    timestamps: true,
+});
+
+export default mongoose.model("Transaction", schema)
